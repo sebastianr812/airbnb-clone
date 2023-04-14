6dda8da65a3393f2d25f70f99ed229bae4232a0c
Sebastian Rojas <sebastianrojas812@gmail.com>
2023-04-14 17:23:13 -0400

    nav bar, register modal, prisma init, auth flow, register functionality

---
+++ app/components/navbar/MenuItem.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+interface MenuItemProps {
+    onClick: () => void;
+    label: string;
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({
+    onClick,
+    label,
+}) => {
+    return (
+        <div className="px-4 py-3 font-semibold transition hover:bg-neutral-100" onClick={onClick}>
+            {label}
+        </div>
+    );
+}
+
+export default MenuItem;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import { Nunito } from 'next/font/google';
 
 import './globals.css'
+import Navbar from './components/navbar/Navbar';
+import ClientOnly from './components/ClientOnly';
+import RegisterModal from './components/modals/RegisterModal';
+import ToasterProvider from './providers/ToasterProvider';
 
 export const metadata = {
   title: 'Airbnb',
@@ -19,7 +23,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
   )
 }
