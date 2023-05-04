8cc650ceffc74cc66a3147f0cd4c926f3317ace8
Sebastian Rojas <sebastianrojas812@gmail.com>
2023-05-04 13:51:07 -0400

    hydration fix

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,7 +2,7 @@ import prisma from '@/app/libs/prismadb';
 
 
 export interface IListingParams {
-    userId?: string | null;
+    userId?: string;
     guestCount?: number;
     roomCount?: number;
     bathroomCount?: number;
--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ const Home = async ({ searchParams }: HomeProps) => {
     <ClientOnly>
       <Container>
         <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-          {listings.map((listing: SafeListing) => (
+          {listings.map((listing: any) => (
             <ListingCard currentUser={currentUser} key={listing.id} data={listing} />
           ))}
 
